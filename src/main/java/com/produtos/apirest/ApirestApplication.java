fe3c6cb64354878bc4c676803a481095b78c069f
Arquimedes Junior <arquimedeslqjunior@gmail.com>
2020-11-25 20:58:09 -0300

    rollback

--- src/main/java/com/produtos/apirest/ApirestApplication.java
+++ src/main/java/com/produtos/apirest/ApirestApplication.java
@@ -2,17 +2,11 @@ package com.produtos.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 public class ApirestApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApirestApplication.class, args);
 	}
-
 }
